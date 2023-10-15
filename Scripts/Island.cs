0ad6f8fe3c8264dea981a7471e0e4b128131585c
Lukas Weber <dev@sakul-flee.de>
2023-10-15 22:31:50 +0200

    CI Trigger

--- Scripts/Island.cs
+++ Scripts/Island.cs
@@ -1,11 +1,16 @@
 using Godot;
 using System;
 
-public partial class Node3D : Godot.Node3D
+public partial class Island : GridMap
 {
+	[ExportGroup("Island")]
+	[Export]
+	public int some_value { get; set; }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("Some Value: " + some_value);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
