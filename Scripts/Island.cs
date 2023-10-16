934a25c0f4543e7193683eef310c62a5422bdb2a
Lukas Weber <dev@sakul-flee.de>
2023-10-16 03:29:08 +0200

    Linux x86_64

--- Scripts/Island.cs
+++ Scripts/Island.cs
@@ -5,7 +5,7 @@ public partial class Island : GridMap
 {
 	[ExportGroup("Island")]
 	[Export]
-	public int some_value { get; set; }
+	private int some_value { get; set; }
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
