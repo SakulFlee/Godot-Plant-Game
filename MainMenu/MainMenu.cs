ebb31b5b586cd5d7d3d041dadb9112d118ce608a
Lukas Weber <dev@sakul-flee.de>
2023-10-24 22:36:43 +0200

    Cleanup

--- MainMenu/MainMenu.cs
+++ MainMenu/MainMenu.cs
@@ -2,13 +2,6 @@ using Godot;
 
 public partial class MainMenu : Control
 {
-	private Button button;
-
-	public override void _Ready()
-	{
-		button = GetNode<Button>("Button");
-	}
-
 	public void OnButtonPressed()
 	{
 		GetTree().ChangeSceneToFile("res:///MainGame/MainGame.tscn");
